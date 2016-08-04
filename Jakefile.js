48b4e2893a972d947dfd0717a8599d5222cbf764
mydoum <a.alonso.giraldo@gmail.com>
2016-08-04 13:01:01 +0200

    Checking node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,3 +1,19 @@
-task('default', function() {
-    console.log("Hello World!");
-});
+(function()
+{
+    desc("Default build");
+
+    task('default', [ "version" ] , function () {
+        console.log("Hello World!");
+    });
+    desc("Check Node version")
+
+    task('version', function () {
+        var packageJson = require("./package.json");
+        var expectedVersion = "v" + packageJson.engines.node;
+        var currVersion = process.version;
+
+        if (expectedVersion !== currVersion) {
+            fail("Node version doesn't match, expected " + expectedVersion + ", but was " + currVersion);
+        }
+    })
+}());
